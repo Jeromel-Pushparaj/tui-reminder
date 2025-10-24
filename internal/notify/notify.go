debc2f99cf3f5c1507581735ce8892608652d49d
Jeromel-Pushparaj <jeromal03@gmail.com>
2025-10-24 15:06:37 +0530

    commit the changes

--- internal/notify/notify.go
+++ internal/notify/notify.go
@@ -1,15 +1,22 @@
 package notify
 
 import (
-	"os/exec"
 	"fmt"
+	"os/exec"
 )
 
-func SendNotification(title, message string) error{
+func SendNotification(title, message string) error {
 	cmd := exec.Command("notify-send", title, message)
-	if err:=cmd.Run(); err != nil {
+	if err := cmd.Run(); err != nil {
 		return fmt.Errorf("failed to send notification: %v", err)
 	}
+	// soundPath, _ := filepath.Abs("/home/jeromel/Public/linuxplayground/tui-reminder/internal/notify/notify_sound.wav")
+	// cmdAplay := exec.Command("aplay", soundPath) // Replace with your sound file path
+	// err := cmdAplay.Run()
+	// if err != nil {
+	// 	return fmt.Errorf("error playing sound with aplay: %v", err)
+	// }
+
 	return nil
 
 }
